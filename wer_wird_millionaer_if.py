fd30799b7611f48fb23f2b9e188232b4f4280fc2
David Potschka <david.baldauf@web.de>
2022-12-19 14:56:07 +0100

    Mehr Fragen.

--- wer_wird_millionaer_if.py
+++ wer_wird_millionaer_if.py
@@ -6,3 +6,39 @@ if antwort1 == "":
     print("RICHTIG!")
 else:
     print("Leider falsch!")
+
+print("200 Frage: ")
+
+antwort1 = input()
+
+if antwort1 == "":
+    print("RICHTIG!")
+else:
+    print("Leider falsch!")
+
+print("300 Frage: ")
+
+antwort1 = input()
+
+if antwort1 == "":
+    print("RICHTIG!")
+else:
+    print("Leider falsch!")
+
+print("500 Frage: ")
+
+antwort1 = input()
+
+if antwort1 == "":
+    print("RICHTIG!")
+else:
+    print("Leider falsch!")
+
+print("1000 Frage: ")
+
+antwort1 = input()
+
+if antwort1 == "":
+    print("RICHTIG!")
+else:
+    print("Leider falsch!")
